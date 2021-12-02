aa0baf2b98f5bf89e1bab9f392d4249a5900d53c
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-12-02 02:55:06 -0500

    #2 Segundo avance - CU - registrar dato psicologo 56%

---
+++ src/app/models/Horario_psicologo.ts
@@ -0,0 +1,6 @@
+export class Horario_psicologo{
+  idhorario:number;
+  idpersonal:number;
+  dia_disponible:string;
+  hora_disponible:string;
+}
--- src/app/services/reg-dato-psicologo.service.ts
+++ src/app/services/reg-dato-psicologo.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { Persona } from '../models/Persona';
+import { Personal_ayuda } from '../models/personal-ayuda';
+import { Horario_psicologo } from '../models/Horario_psicologo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegDatoPsicologoService {
 
-  constructor() { }
+  constructor(private http:HttpClient,private auth:AuthService) { }
+
+  urlEndpoint = "http://localhost:5050/EX3/datos_psicologo";
+
+  crear_datos_psicologo(persona:Persona,personal_ayuda:Personal_ayuda,horario_psicologo:Horario_psicologo):Observable<String>{
+    console.log(persona)
+    console.log(personal_ayuda)
+    console.log(horario_psicologo)
+    return this.http.post<String>(this.urlEndpoint + '/register/dato_psicogolo',{persona,personal_ayuda,horario_psicologo});
+  }
 }
